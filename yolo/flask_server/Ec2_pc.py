6ff28954efcb80c642685016aa7bc002a7609489
QiXu <qi.xu@engie.com>
2018-06-14 11:14:31 +0200

    change port

--- yolo/flask_server/Ec2_pc.py
+++ yolo/flask_server/Ec2_pc.py
@@ -52,4 +52,4 @@ def test():
     return Response(response=response_pickled, status=200, mimetype="application/json")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=5000)
